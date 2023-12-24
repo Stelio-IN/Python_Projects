5a15539cd4cfad8ae4e7af1ad077d662d93d7140
StelioAMIN <stelioinvolution@gmail.com>
2023-12-24 17:43:43 +0200

    add

--- First/Interface/telas_intera.py
+++ First/Interface/telas_intera.py
@@ -1,12 +1,29 @@
 from tkinter import *
+#
+# def create_window():
+#     #new_window = Toplevel() # new window on top of other windows! Associate window
+#     new_window = Tk()
+#     old_window.destroy()
+#
+# old_window = Tk()
+#
+# button = Button(text='create new Window', command=create_window).pack()
+#
+# old_window.mainloop()
 
-def create_window():
-    #new_window = Toplevel() # new window on top of other windows! Associate window
-    new_window = Tk()
-    old_window.destroy()
 
-old_window = Tk()
+def abrir_janela():
+    janela2 = Tk()
+    janela2.title("Secundaria")
+ #   janela.destroy()
+    label = Label(janela2,text="Nome:").grid(row=0,column=0)
+    txt = Entry(janela2).grid(row=0,column=1)
+    label1 = Label(janela2,text="senha:").grid(row=1,column=0)
+    txt1 = Entry(janela2).grid(row=1,column=1)
+    button = Button(janela2,text="Voltar",command=janela2.destroy).grid(row=2,column=1)
 
-button = Button(text='create new Window', command=create_window).pack()
+janela = Tk()
 
-old_window.mainloop()
+button = Button(janela, text="Outra tela",command=abrir_janela)
+button.grid(row=0,column=0)
+janela.mainloop()
